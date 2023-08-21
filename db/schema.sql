6065d0548577b79b7470eec1b4c4f1b8b3a21a05
Naomi-Pate <patenaomi99@gmail.com>
2023-08-20 23:36:49 -0400

    fixed typo in db

--- db/schema.sql
+++ db/schema.sql
@@ -6,13 +6,13 @@ DROP TABLE IF EXISTS tickets;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE tickets (
-    id SERIAL PRIMARY KEY NOT NULL,
-    date NOT NULL,
-    title TEXT NOT NULL,
-    description TEXT NOT NULL,
-    priority TEXT NOT NULL,
-    assigned TEXT,
-    resolution BOOLEAN,
-    username TEXT NOT NULL,
-    unique_id uuid DEFAULT uuid_generate_v4 ()
+    id SERIAL PRIMARY KEY NOT NULL, 
+    date TEXT, 
+    title TEXT NOT NULL, 
+    description TEXT NOT NULL, 
+    priority TEXT NOT NULL, 
+    assigned TEXT, 
+    resolution BOOLEAN, 
+    username TEXT NOT NULL, 
+    unique_id uuid DEFAULT uuid_generate_v4()
 );
